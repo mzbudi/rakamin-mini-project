4259400b63486dcdc53ff40d15f0ca02696c2371
mzbudi <ronibudianto361@gmail.com>
2023-01-07 16:32:47 +0700

    Add : New Task Modal

--- src/components/Kanban/KanbanGroup.jsx
+++ src/components/Kanban/KanbanGroup.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import KanbanCard from "./KanbanCard";
+import Modal from "../Modal";
 
 const KanbanGroup = ({ kanbanColor }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [input, setInput] = useState({
+    taskName: "",
+    progress: "",
+  });
+
+  const handleInput = (e, field) => {
+    setInput({ ...input, [field]: e.target.value });
+  };
+
+  const modalConfig = {
+    title: "Create Task",
+    content: (
+      <>
+        <p className="text-xs leading-5 font-bold text-modalContent font-nunito mt-4">
+          Task Name
+        </p>
+        <input
+          type="text"
+          placeholder="Type your task"
+          className="w-full border-2 border-[#EDEDED] rounded-lg px-4 py-2 mt-2 text-xs leading-5 opacity-50"
+          onChange={(e) => handleInput(e, "taskName")}
+          value={input.taskName}
+        />
+        <p className="text-xs leading-5 font-bold text-modalContent font-nunito mt-4">
+          Progress
+        </p>
+        <input
+          type="text"
+          placeholder="70%"
+          className="border-2 border-[#EDEDED] rounded-lg px-4 py-2 mt-2 text-xs leading-5 opacity-50"
+          onChange={(e) => handleInput(e, "progress")}
+          value={input.progress}
+        />
+      </>
+    ),
+    button: {
+      cancel: {
+        text: "Cancel",
+        onClick: () => {
+          setIsOpen(false);
+        },
+      },
+      secondary: {
+        button: (
+          <button
+            type="button"
+            className="flex flex-row justify-center ml-[5px] rounded-md shadow-sm border border-borderPrimary bg-primary px-4 py-2 text-sm font-medium text-white hover:bg-borderPrimary focus:outline-none focus-visible:ring-2 focus-visible:ring-borderPrimary focus-visible:ring-offset-2"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          >
+            <p className="font-nunito text-sm leading-6 text-white">
+              Save Task
+            </p>
+          </button>
+        ),
+        onClick: () => {
+          setIsOpen(false);
+        },
+      },
+    },
+  };
   // DropDown Toggle
   // const dropDownToggle = () => {};
 
@@ -26,12 +90,12 @@ const KanbanGroup = ({ kanbanColor }) => {
         </p>
         {/* Card Kanban */}
         <KanbanCard progress={60} />
-        <KanbanCard progress={100}/>
-        <KanbanCard progress={10}/>
+        <KanbanCard progress={100} />
+        <KanbanCard progress={10} />
 
         {/* Button */}
         <div className="flex flex-col mt-2">
-          <button className="flex items-center w-fit">
+          <button className="flex items-center w-fit" onClick={()=>setIsOpen(true)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -52,6 +116,7 @@ const KanbanGroup = ({ kanbanColor }) => {
           </button>
         </div>
       </div>
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} modalConfig={modalConfig} />
     </div>
   );
 };
